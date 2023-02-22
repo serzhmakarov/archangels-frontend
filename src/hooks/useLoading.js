1af5489d51f88e10843bf5b0dec407ff677d75fd
Сергей Макаров <smakarov@MacBook-Pro-Sergej-Makarov-7.local>
2023-02-22 05:25:09 +0300

    Add loading gif spinner

--- src/hooks/useLoading.js
+++ src/hooks/useLoading.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import LoadingGif from '../assets/images/loading.gif';
 
 const withLoading = Component => (props) => {
 	if (!props.isLoaded) {
-		return (<div>Loading...</div>);
+		return (
+			<div className="loader-spinner">
+				<img src={LoadingGif} alt="Loading..." />
+			</div>
+		);
 	}
 
 	return <Component {...props} />
