a479606be5a148245c3f2b9056357c2a64224f34
Сергей Макаров <smakarov@MacBook-Pro-Sergej-Makarov-7.local>
2023-03-01 22:32:00 +0300

    heroku buildpacks

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 const API_URL = 'http://localhost:3000/api/v1';
 // const API_URL = `${process.env.REACT_APP_BASE_API_URL}/api/v1`;
 
+
 const endpoints = {
   posts: '/posts/',
   reports: '/reports/',
