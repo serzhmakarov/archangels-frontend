b518c5bb3274929e55f8773c90e1f2fafad8d40a
Serhii Makarov <serhii.makarov@cropio.com>
2023-06-18 20:08:35 +0300

    Generate sitemap. Update robots.txt

---
+++ generate-sitemap.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const { SitemapStream, streamToPromise } = require('sitemap');
+const { Readable } = require('stream');
+
+const routesPath = {
+  root: '/',
+  about: '/about',
+  help: '/help',
+  reports: '/reports',
+  documents: '/documents',
+  terms: '/terms',
+  policy: '/policy',
+  refund: '/refund-policy',
+  partners: '/partners',
+};
+
+const links = Object.values(routesPath).map(path => ({ url: path, changefreq: 'daily', priority: 0.9 }));
+
+const stream = new SitemapStream({ hostname: 'https://www.archangels.in.ua' });
+
+streamToPromise(Readable.from(links).pipe(stream)).then((data) =>
+  fs.writeFile('public/sitemap.xml', data.toString(), function(err) {
+    if (err) throw err;
+    console.log('Sitemap.xml was created!');
+  })
+);
